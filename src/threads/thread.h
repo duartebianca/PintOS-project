36df87d2f1bfbaea6e93c668ada5f93e7b1d28f2
duartebianca <biancaduarte1914@gmail.com>
2025-03-12 09:12:21 -0300

    refactor: Remove unused priority comparison function from thread header

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -130,7 +130,7 @@ void thread_print_stats(void);
 
 typedef void thread_func(void *aux);
 tid_t thread_create(const char *name, int priority, thread_func *, void *);
-//bool comp_priority(const struct list_elem *a, const struct list_elem *b, void *aux);
+
 
 
 void thread_block(void);
